c6d2a2f6b8de4f9c9492db5f279e0bf5d9e80cf6
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-29 16:09:56 +0700

    update KontakCard on HomeScreen

--- app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -77,38 +78,42 @@ fun KontakLayout(kontak: List<Kontak>, modifier: Modifier = Modifier){
 @Composable
 fun KontakCard(
     kontak: Kontak,
+    onDeleteClick: (Kontak) -> Unit = {},
     modifier: Modifier = Modifier
-){
-  Card(
-      modifier = modifier,
-      shape = MaterialTheme.shapes.medium,
-      elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-  )  {
-      Column(
-          modifier = Modifier.padding(16.dp),
-          verticalArrangement = Arrangement.spacedBy(8.dp)
-      ){
-          Row(
-              modifier = Modifier.fillMaxWidth(),
-          ){
-              Text(
-                  text = kontak.nama,
-                  style = MaterialTheme.typography.titleLarge,
-              )
-              Spacer(Modifier.weight(1f))
-              Icon(
-                  imageVector = Icons.Default.Phone,
-                  contentDescription = null,
-              )
-              Text(
-                  text = kontak.telpon,
-                  style = MaterialTheme.typography.titleMedium
-              )
-              Text(
-                  text = kontak.alamat,
-                  style = MaterialTheme.typography.titleMedium,
-              )
-          }
-      }
-  }
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = kontak.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(kontak) }) {
+                    Icon(
+                        imageVector = Icons.Default.Phone,
+                        contentDescription = null,
+                    )
+                    Text(
+                        text = kontak.telpon,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = kontak.alamat,
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                }
+            }
+        }
+    }
 }
