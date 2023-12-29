1fa59854de9b69ac67a096c1dcf4629d4be02c90
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-29 15:56:23 +0700

    create fun TopAppBarKontak, center AlignedTopAppBar

--- app/src/main/java/com/example/kontak/ui/KontakApp.kt
+++ app/src/main/java/com/example/kontak/ui/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.kontak.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -64,4 +68,16 @@ fun TopAppBarKontak(
     scrollBehavior: TopAppBarScrollBehaviour? = null,
     navigateUp: () -> Unit = {}
     ){
+    CenterAlignedTopAppBar(title = {Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehaviour,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "")
+                }
+            }
+        })
 }
