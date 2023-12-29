c6d2e79912cd99b7e59ee6788ba5d770485e9884
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-29 16:21:38 +0700

    create fun homescreen

--- app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
@@ -15,23 +15,49 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kontak.R
 import com.example.kontak.model.Kontak
+import com.example.kontak.navigation.DestinasiNavigasi
+import com.example.kontak.ui.PenyediaViewModel
+import com.example.kontak.ui.TopAppBarKontak
+import com.example.kontak.ui.home.viewmodel.HomeViewModel
 import com.example.kontak.ui.home.viewmodel.KontakUIState
 
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "kontak"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
 @Composable
 fun HomeStatus(
     kontakUIState: KontakUIState,
