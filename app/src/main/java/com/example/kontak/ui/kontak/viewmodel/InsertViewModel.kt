93c948bda9022416ab12866517bd24101d7ccd7e
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-29 16:44:05 +0700

    create val and fun on InsertViewModel

--- app/src/main/java/com/example/kontak/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.kontak.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.kontak.Repositori.KontakRepositori
+
+class InsertViewModel (private val kontakRepository: KontakRepositori) : ViewModel() {
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent : InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepositori.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
